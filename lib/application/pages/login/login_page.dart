87e117e92e571309b1c8648ecdb51adae5a8ed56
MauBocanegraTala <mauro.bocanegra@tala.co>
2024-03-07 10:04:13 -0600

    custom login start

--- lib/application/pages/login/login_page.dart
+++ lib/application/pages/login/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:centinelas_app/application/core/strings.dart';
 import 'package:centinelas_app/application/pages/login/widgets/google_sign_in_button.dart';
 import 'package:flutter/material.dart';
 
@@ -45,9 +46,36 @@ class LoginPageState extends State<LoginPage> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Image.asset('assets/icon/icon.png'),
-                  const SizedBox(height: 24,),
-                  GoogleSignInButton()
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Image.asset('assets/icon/icon.png'),
+                  ),
+                  const SizedBox(height: 48,),
+                  GoogleSignInButton(),
+                  TextButton(
+                      onPressed: (){},
+                      child: const Text.rich(
+                        TextSpan(
+                          text: privacyButtonText1,
+                            style: TextStyle(
+                              fontSize: 12.0,
+                              color: Colors.grey,
+                            ),
+                          children: <TextSpan>[
+                            TextSpan(
+                                text: privacyButtonText2,
+                                style: TextStyle(
+                                  fontSize: 12.0,
+                                  color: Colors.grey,
+                                  decoration: TextDecoration.underline,
+                                )
+                            ),
+                            // can add more TextSpans here...
+                          ],
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                  ),
                 ],
               ),
             ),
