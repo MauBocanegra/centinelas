892b418eec52f60138bc598ff37688e611112937
macstudiosp <sportspromotion.com.mx@gmail.com>
2024-06-04 15:10:05 -0600

    permissions in dispatch top page

--- lib/application/pages/dispatch/dispatch_page.dart
+++ lib/application/pages/dispatch/dispatch_page.dart
@@ -1,8 +1,17 @@
 import 'package:centinelas_app/application/core/page_config.dart';
 import 'package:centinelas_app/application/core/routes_constants.dart';
+import 'package:centinelas_app/application/di/injection.dart';
 import 'package:centinelas_app/application/pages/incidences/incidence_page.dart';
+import 'package:centinelas_app/application/pages/login/login_page.dart';
 import 'package:centinelas_app/application/pages/users_list/users_list.page.dart';
+import 'package:centinelas_app/core/usecase.dart';
+import 'package:centinelas_app/domain/usecases/write_dispatcher_usecase.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:go_router/go_router.dart';
 
 class DispatchPage extends StatefulWidget {
   final String raceId;
@@ -41,8 +50,44 @@ class DispatchPageState extends State<DispatchPage> {
     });
   }
 
+  void checkNotifsPermissions() async {
+    NotificationSettings settings =
+    await serviceLocator<FirebaseMessaging>().requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+    debugPrint('User granted permission: ${settings.authorizationStatus}');
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    SchedulerBinding.instance.addPostFrameCallback((timeStamp) async {
+      if(serviceLocator<FirebaseAuth>().currentUser != null){
+        try{
+          checkNotifsPermissions();
+          final writeDispatcherUseCase =
+          serviceLocator<WriteDispatcherUseCase>();
+          final dispatcherWasWritten =
+          await writeDispatcherUseCase.call(NoParams());
+          if(dispatcherWasWritten.isRight){
+            throw Exception('Unable to write dispatcher in RTDB');
+          }
+        }on Exception catch(e){
+          serviceLocator<FirebaseCrashlytics>().recordError(e, null);
+          debugPrint('error inSessionPage ${e.toString()}');
+        }
+      } else {
+        debugPrint('shouldGoToLogin');
+        context.go('/${LoginPage.pageConfig.name}');
+      }
+    });
+
     List pages = [
       IncidencesPageProvider(
         raceId: widget.raceId,
